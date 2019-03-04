cb906504070597a038b2e414693122c31dca7963
SketchAlgorithm <47266632+SketchAlgorithm@users.noreply.github.com>
2019-03-04 23:57:10 +0900

    Create 가장 긴 증가하는 부분 수열.cpp

---
+++ 가장 긴 증가하는 부분 수열.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	int size;
+	cin >> size;
+	vector<int> seq(size);
+	// 수열 입력
+	for (int i = 0; i < size; i++) {
+		cin >> seq[i];
+	}
+
+	// 수열 내 비교 후 지울 수열 지우기
+	for (int i = 0; i < seq.size()-1; i++) {
+		if (seq[i] >= seq[i + 1]) {
+			seq.erase(seq.begin() + (i + 1));
+			i--;
+		}
+	}
+
+	cout << seq.size() << endl;
+	for (int i = 0; i < seq.size(); i++)
+		cout << seq[i] << " ";
+
+	return 0;
+}
